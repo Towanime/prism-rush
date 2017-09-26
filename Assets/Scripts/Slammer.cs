6e77ee61e067ce1edbd5bd967770984a4a850bf4
Jay <jayguzzo@gmail.com>
2017-09-26 00:50:32 -0400

    Slammer will chase the player based on its defined speed. Will only chase if player enters the minimum distance, and will stop chasing if the player manages to get far enough away (max distance)

---
+++ Assets/Scripts/Slammer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slammer : MonoBehaviour {
+
+	//Setting up the player as the Slammer's target
+	public GameObject target;
+	//The minimum distance the player needs to be for the Slammer to chase her
+	public float minDistance;
+	//Max distance before the Slammer stops chasing
+	public float maxDistance;
+	
+	//Slammer stats (speed and rotation)
+	public float speed;
+	public float rotationSpeed;
+
+	//Starting transform for reference
+	private Transform startingTransform;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Player");
+		startingTransform.position = this.transform.position;
+		startingTransform.rotation = this.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float currentDistance = Vector3.Distance (target.transform.position, this.transform.position);
+		if (currentDistance < minDistance) {
+			this.transform.position = Vector3.Slerp (this.transform.position, target.transform.position, speed * Time.deltaTime);
+			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, target.transform.rotation, rotationSpeed * Time.deltaTime);
+		} else if (currentDistance > maxDistance) {
+			this.transform.position = Vector3.Lerp (this.transform.position, startingTransform.position, speed);
+			this.transform.rotation = Quaternion.Lerp (this.transform.rotation, startingTransform.rotation, rotationSpeed);
+		}
+	}
+}
