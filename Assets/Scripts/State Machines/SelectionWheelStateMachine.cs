0e8fdd6b36413281c78328fdd0ab9ef94c2bd6bf
Dom <dominiklussier@gmail.com>
2017-09-28 20:57:02 -0400

    work on the UI state machine

---
+++ Assets/Scripts/State Machines/SelectionWheelStateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterLove.StateMachine;
+
+public class SelectionWheelStateMachine : MonoBehaviour {
+
+	public PlayerInput playerInput;
+
+	private StateMachine<AimStates> fsm;
+	private bool triggerPressed;
+
+	public StateMachine<WheelStates> StateMachine
+	{
+		get
+		{
+			if (fsm == null)
+			{
+				fsm = StateMachine<AimStates>.Initialize(this, AimStates.Default);
+			}
+			return fsm;
+		}
+	}
+}
--- Assets/Scripts/UI/SelectionWheelStateMachine.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SelectionWheelStateMachine : MonoBehaviour {
-
-	public PlayerInput playerInput;
-	private bool triggerPressed;
-}
