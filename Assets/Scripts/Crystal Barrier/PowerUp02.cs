24c912a44e934aad43d779f4e8ee8e634881c275
Benson <bepdesign5@gmail.com>
2017-10-12 15:21:51 -0400

    updated the powerup script

--- Assets/Scripts/Crystal Barrier/PowerUp02.cs
+++ Assets/Scripts/Crystal Barrier/PowerUp02.cs
@@ -6,10 +6,12 @@ public class PowerUp02 : MonoBehaviour {
 
 	HealthCasters hd;
 	public float invincibleTime = 1.0f;
+	private MeshRenderer Mrender;
 
 	void Start()
 	{
 		StartCoroutine(NoDamage());
+		Mrender = GetComponent<MeshRenderer> ();
 	}
 
 
@@ -19,20 +21,29 @@ public class PowerUp02 : MonoBehaviour {
 		{ 
 			hd = other.gameObject.GetComponent<HealthCasters>();
 			StartCoroutine(NoDamage());
-			GetComponent<SpriteRenderer>().enabled = false;
+			GetComponent<MeshRenderer>().enabled = false;
 			GetComponent<BoxCollider2D>().enabled = false;
 		}
 	}
 
-	IEnumerator NoDamage () 
+	public IEnumerator NoDamage () 
 	{
 		hd.enabled = false;
+		StartCoroutine(Flicker());
 		invincibleTime -= Time.deltaTime;
 		Debug.Log ("Health Disabled " + hd.currentHealth);
-		yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds(invincibleTime);
 		hd.enabled = true;
 		Debug.Log ("Health Enabled " + hd.currentHealth);
 		Destroy(this);
 
+	}
+
+	private IEnumerator Flicker()
+	{
+		Mrender.enabled = false;
+		yield return new WaitForSeconds(.1f);
+		Mrender.enabled = true;
+		yield return new WaitForSeconds(.1f);
 	}
 }
