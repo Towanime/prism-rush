808fd7ef57f996e3e23b46d8de65b18b1b2c4f6d
Viktor Briceno <viktor.briceno@gmail.com>
2017-10-16 17:07:04 -0400

    Enemy touch hazard implemented, slime prefab changes and player score implemented.

---
+++ Assets/Scripts/Entities/Enemies/EnemyTouchHazard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTouchHazard : Hazard
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        Score scoreComponent = collision.gameObject.GetComponent<Score>();
+        if (collision.gameObject.name == "Player" && scoreComponent)
+        {
+            scoreComponent.OnDamage(collision.gameObject, damage);
+        }
+
+    }
+    
+}
---
+++ Assets/Scripts/Entities/Score.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [Tooltip("Initial score points for this entity.")]
+    public int initialScore;
+    [Tooltip("True to make this entity ignore any damage.")]
+    public bool ignoreDamage = false;
+    // current hp
+    protected int currentScore;
+
+    void Start()
+    {
+        currentScore = initialScore;
+    }
+
+    /// <summary>
+    /// Entity takes specified damage and may trigger OnDeath method if necessary.
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="damage"></param>
+    /// <returns></returns>
+    public virtual bool OnDamage(GameObject origin, float damage)
+    {
+        if (ignoreDamage) return false;
+        ModifyCurrentLife(damage);
+        if (currentScore <= 0)
+        {
+            OnDeath();
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Substract damage from the current health value.
+    /// </summary>
+    /// <param name="damage"></param>
+    protected virtual void ModifyCurrentLife(float damage)
+    {
+        currentScore = (int) Mathf.Max(currentScore - damage, 0);
+    }
+
+    /// <summary>
+    /// Called upon healh reaching a number equal or less than 0.
+    /// </summary>
+    protected virtual void OnDeath()
+    {
+        Debug.Log("Player Dead");
+        //Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Restores current health and makes it equal to initial health.
+    /// </summary>
+    public virtual void Refresh()
+    {
+        currentScore = initialScore;
+    }
+   
+    /// <summary>
+    /// Returns or sets the initial health of this entity.
+    /// </summary>
+    public int InitialScore
+    {
+        get { return initialScore; }
+        set { initialScore = value; }
+    }
+
+    /// <summary>
+    /// Returns or sets the current heatlh for this entity.
+    /// </summary>
+    public int CurrentScore
+    {
+        get { return currentScore; }
+        set { currentScore = value; }
+    }
+}
