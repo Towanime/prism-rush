65ccf1f7af339f36e0ad7769074c76e85388f8ed
recksbayne <recksbayne@gmail.com>
2017-10-16 18:26:10 -0400

    Start Screen

---
+++ Assets/Scripts/UI/StartScreenButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // Required when using Event data.
+
+public class StartScreenButton : MonoBehaviour, IPointerDownHandler // required interface when using the OnPointerDown method.
+{
+
+	public StartScreenTransition startScreenTransition;
+
+	public GameObject blackBackground;
+
+	public void OnPointerDown (PointerEventData eventData) 
+	{
+		startScreenTransition.transition = true;
+	}
+
+	public void animEnd(){
+		blackBackground.GetComponent<Animator> ().enabled = true;
+	}
+}
