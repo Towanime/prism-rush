6d5188b18edbe4e8e08129e6b5f70de1f6e00fa6
Jay <jayguzzo@gmail.com>
2017-10-16 18:42:03 -0400

    Getting hit by the Slammer subtracts from score

--- Assets/Scripts/Entities/Enemies/Slammer/SlammerCollider.cs
+++ Assets/Scripts/Entities/Enemies/Slammer/SlammerCollider.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class SlammerCollider : MonoBehaviour {
 
+	private GameObject player;
+
+	public int score;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		score = player.GetComponent<Score>().CurrentScore;
+	} 
+
 	void OnTriggerEnter (Collider other) {
 		if (other.tag == "Player") {
 			Debug.Log ("Player Stunned!");
+			score -= 10;
+			Debug.Log(score);
 		}
 	}
 }
