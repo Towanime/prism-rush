19e23eda78ab7aa05e60a054d1f7f3a0b16da184
Benson <bepdesign5@gmail.com>
2017-10-17 18:17:03 -0400

    still working on it

---
+++ Assets/Scripts/Crystal Barrier/GiveDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveDamage : MonoBehaviour 
+{
+
+	HealthCasters HC;
+
+	void Start()
+	{
+		HC = GetComponent<HealthCasters> ();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Enemy") {
+			HC.currentHealth -= 1;
+			
+		}
+	}
+}
