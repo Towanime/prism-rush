5691a87aa905bc1c0dfb3e3021288ae5ff28ad77
Jay <jayguzzo@gmail.com>
2017-10-19 16:53:54 -0400

    High score display

---
+++ Assets/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+	private GameObject player;
+	private Score score;
+
+	private int highScore;
+	public Text highScoreText;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player"); //Fine the GameObject tagged "Player" and assign it to the player var
+		score = player.GetComponent<Score>(); //Grab the Score component from player and assign it to the score var
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (score.currentScore > highScore) {
+			highScore = score.currentScore;
+			PlayerPrefs.SetInt ("HighScore", highScore);
+		}
+
+		highScoreText.text = PlayerPrefs.GetInt ("HighScore", highScore).ToString();
+	}
+}
