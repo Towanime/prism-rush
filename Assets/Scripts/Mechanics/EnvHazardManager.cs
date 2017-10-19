dc9f6032d4a5ffd180a888dbfba3de7a423e0c4e
Jay <jayguzzo@gmail.com>
2017-10-19 00:47:13 -0400

    Pressing H starts the audio clip and at the end the hazard turns on.

--- Assets/Scripts/Mechanics/EnvHazardManager.cs
+++ Assets/Scripts/Mechanics/EnvHazardManager.cs
@@ -6,27 +6,27 @@ public class EnvHazardManager : MonoBehaviour {
 
 	public GameObject envHazard;
 
-	private AudioSource audio;
+	private AudioSource audioClip;
 
 	void Start () {
-		audio = GetComponent<AudioSource>();
-		audio.enabled = false;
+		audioClip = GetComponent<AudioSource>();
+		audioClip.enabled = false;
 	}
 
 	void Update () {
 		//If the player presses H, enable and disable the environmental hazard
 		if (Input.GetKeyDown (KeyCode.H) && envHazard.activeInHierarchy == false) {
-			/* envHazard.SetActive (true); */ 
-       		audio.enabled = true;
-			audio.Play();
-        	audio.Play(44100);
-			Debug.Log(audio.isPlaying);
-			if (audio.isPlaying == false) {
-				envHazard.SetActive (true);
-			}
+       		audioClip.enabled = true;
+			audioClip.Play ();
+			Invoke ("TurnOnHazard", audioClip.clip.length);
+			
 
 		} else if (Input.GetKeyDown (KeyCode.H) && envHazard.activeInHierarchy == true) {
 			/* envHazard.SetActive (false); */
 		}
+	}
+
+	void TurnOnHazard () {
+		envHazard.SetActive (true);
 	}
 }
