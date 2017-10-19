b35049fb85e1c70f1713b77d0acd1019f40dc727
Jay <jayguzzo@gmail.com>
2017-10-19 00:32:23 -0400

    Working on the audio environment hazard, and Slammer prefab done (health can be adjusted as needed)

--- Assets/Scripts/Mechanics/EnvHazardManager.cs
+++ Assets/Scripts/Mechanics/EnvHazardManager.cs
@@ -6,12 +6,27 @@ public class EnvHazardManager : MonoBehaviour {
 
 	public GameObject envHazard;
 
+	private AudioSource audio;
+
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		audio.enabled = false;
+	}
+
 	void Update () {
 		//If the player presses H, enable and disable the environmental hazard
 		if (Input.GetKeyDown (KeyCode.H) && envHazard.activeInHierarchy == false) {
-			envHazard.SetActive (true);
+			/* envHazard.SetActive (true); */ 
+       		audio.enabled = true;
+			audio.Play();
+        	audio.Play(44100);
+			Debug.Log(audio.isPlaying);
+			if (audio.isPlaying == false) {
+				envHazard.SetActive (true);
+			}
+
 		} else if (Input.GetKeyDown (KeyCode.H) && envHazard.activeInHierarchy == true) {
-			envHazard.SetActive (false);
+			/* envHazard.SetActive (false); */
 		}
 	}
 }
